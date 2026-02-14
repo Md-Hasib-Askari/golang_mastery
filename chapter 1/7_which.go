d687b4ff9d8054e1a45d18561eeac430eb2e2845
hasib <mdhsbaskari@gmail.com>
2026-02-14 22:07:15 +0600

    Add 'which' function to locate executable files in system PATH

---
+++ chapter 1/7_which.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func which() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		println("Usage: go run 7_which.go <command>")
+		return
+	}
+
+	file := arguments[1]
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+
+	for _, p := range pathSplit {
+		fullPath := filepath.Join(p, file)
+		fileInfo, err := os.Stat(fullPath)
+		if err != nil {
+			println(fullPath)
+			continue
+		}
+
+		mode := fileInfo.Mode()
+		if !mode.IsRegular() {
+			continue
+		}
+
+		if mode&0111 != 0 {
+			println(fullPath)
+			return
+		}
+
+	}
+}
