e683b8f5fbb4384cfc0519d73faa7a87bfea9ec5
hasib <mdhsbaskari@gmail.com>
2026-02-14 22:16:50 +0600

    Add custom logging example with multiple log destinations

---
+++ chapter 1/8_customLog.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"path"
+)
+
+func main() {
+	LOGFILE := path.Join(os.TempDir(), "mGo.log")
+
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	LstdFlags := log.Ldate | log.Ltime | log.Lshortfile
+
+	iLog := log.New(f, "iLog ", LstdFlags)
+
+	iLog.Println("This is an info log")
+
+	// multiple log destinations
+	w := io.MultiWriter(f, os.Stderr)
+	logger := log.New(w, "multiLog ", LstdFlags)
+	logger.Println("This is a multi log")
+}
